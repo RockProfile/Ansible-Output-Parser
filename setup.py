ac2a70fd260d34f3782e30e591dd445fb399b9cf
Peter McDonald <me@petermcdonald.co.uk>
2021-03-06 15:36:22 +0000

    Corrections

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 
 PACKAGE_NAME = 'ansible-output-parser'
-PACKAGE_VERSION = '0.0.3'
+PACKAGE_VERSION = '0.0.4'
 DESCRIPTION = 'Parser for Ansible output'
-with open('README', 'r') as fileHandler:
+with open('README.rst', 'r') as fileHandler:
     LONG_DESCRIPTION = fileHandler.read()
 
 TEST_REQUIRES = [
@@ -18,25 +18,21 @@ TEST_REQUIRES = [
 setup(
     name=PACKAGE_NAME,
     version=PACKAGE_VERSION,
-    url='',
-    download_url='',
-    project_urls={},
+    url='https://github.com/RockProfile/Ansible-Output-Parser',
     author='Peter McDonald',
     author_email='admin@rockprofile.com',
     maintainer='Peter McDonald',
     maintainer_email='admin@rockprofile.com',
-    classifiers='',
+    classifiers='Development Status :: 4 - Beta',
     license='MIT',
     license_file='LICENSE',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='',
+    long_description_content_type='text/x-rst',
     keywords='ansible',
     packages=find_packages(),
     package_dir={'ansible_parser': 'ansible_parser'},
     platforms='',
-    provides='',
-    requires='',
+    provides='ansible_parser',
     extras_require={'test': TEST_REQUIRES},
-    obsoletes='',
 )
