f536bb1dae16b3aa4d4eedfd26196be7318e2a6e
Jan Hutar <jhutar@redhat.com>
2022-09-09 15:24:35 +0200

    Add method to list all plays and all tasks

--- test/test_task.py
+++ test/test_task.py
@@ -38,26 +38,7 @@ def test_task_results_count(filename, expected_all, expected_failures):
         file_content = handler.read()
     task = Tasks(file_content)
 
-    assert len(task.results) == expected_all
-    assert len(task.failures) == expected_failures
-
-
-@pytest.mark.xfail
-@pytest.mark.parametrize(
-    "filename",
-    [
-        'task_with_command.txt',
-        'task_with_debug_msg.txt',
-        'task_with_debug_var.txt',
-    ],
-)
-def test_skipping_debug(filename):
-    with open(f'test/test_data/tasks/{filename}', 'r') as handler:
-        file_content = handler.read()
-    try:
-        _ = Tasks(file_content)
-    except IndexError as e:
-        pytest.fail(f'Failed with IndexError: {e}')
+    assert task.has_failures == has_failures
 
 
 @pytest.mark.parametrize(
